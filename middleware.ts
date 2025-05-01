4bc9bfc1285df6104b8de60d7100e140c890f06a
Vibhanshu Pandey <vibhanshu2207@gmail.com>
2025-05-01 21:55:02 +0530

    added all-catch route for sign-in for smoother functionality

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-//public routes
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
@@ -11,9 +10,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
+    "/((?!_next|.*\\.(?:.*)).*)", // Match all but _next and static files
     "/(api|trpc)(.*)",
   ],
 };
