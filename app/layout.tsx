1aa752240eb8771715fa2c48c7183ee748f013c2
Vibhanshu Pandey <vibhanshu2207@gmail.com>
2025-05-01 21:43:21 +0530

    Added authentication to admin panel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,43 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
   description: "A clean admin dashboard for managing student records",
-    generator: 'MAIT'
-}
+  generator: "MAIT",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-      </head>
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        </head>
+        <body className={inter.className}>
+          <SignedIn>
+            <header className="flex justify-end items-center p-4 gap-4 h-16">
+              <UserButton />
+            </header>
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  experimental: {
+    serverActions: {},
+  },
+};
 
-export default nextConfig
+export default nextConfig;
